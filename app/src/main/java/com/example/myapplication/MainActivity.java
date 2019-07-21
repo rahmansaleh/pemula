b3240ab70819b858a74e84c8c6b9820701c763cd
muhammad rahman <rahman9600@gmail.com>
2019-07-21 16:58:20 +0700

    trial refactor code and instance state
    refactor code using command+option+L /  ctrl+shift/alt+L

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,9 +5,42 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String data = "Hello World";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (true) {
+            data = "Halo Dunia";
+        } else {
+            abc();
+
+        }
+
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    private int abc() {
+
+
+        return 1;
+    }
+
+    private boolean AA() {
+
+        return false;
+    }
+
+
 }
